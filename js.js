f4b0e80e810ef851b66a6a74d177ea88ec7c9a11
Yaroslav-Teplyi <57116311+Yaroslav-Teplyi@users.noreply.github.com>
2021-03-01 22:27:13 +0300

    tsk 2 0.5

--- js.js
+++ js.js
@@ -26,3 +26,41 @@ longWord('1')
 
 console.log('Task 2---------------------')
 
+function capsLock(str) {
+    let splitedString = str.split(' ')
+
+    for(let i = 0; i < splitedString.length; i++) {
+
+        let splitedLetters = splitedString[i].split('')
+
+        if(splitedLetters[0].match('[a-z]')) {
+            for(let y = 1; y < splitedLetters.length; y++) {
+                if(splitedString[i].slice(1).match('[a-z]')) {
+                    console.log(splitedString[i]) 
+                    break
+                } else if(splitedString[i].slice(1).match('[A-Z]')) {
+                    console.log(`${splitedLetters[0].toUpperCase()}${splitedString[i].slice(1).toLowerCase()}`)
+                    break
+                }
+            }
+        } else if(splitedLetters[0].match('[A-Z]')) {
+            for(let y = 1; y < splitedLetters.length; y++) {
+                if(splitedString[i].slice(1).match('[a-z]')) {
+                    console.log(splitedString[i]) 
+                    break
+                } else if(splitedString[i].slice(1).match('[A-Z]' && splitedString[i].length > 2)){
+                    console.log(`${splitedLetters[0].toUpperCase()}${splitedString[i].slice(1).toLowerCase()}`) 
+                    break
+                } else {
+                    console.log(splitedString[i].toLowerCase())
+                    break
+                }
+            }
+        }     
+    }
+}
+
+capsLock('cAPS')
+capsLock('Lock')
+capsLock('wHY DO wE NEED cAPS lOCK?')
+capsLock('FuNkY iS nOt CaPs!')
